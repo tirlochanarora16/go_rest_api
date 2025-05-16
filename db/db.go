cf40b4032c8b2ba5c4b4705a489c8f24a91d30e8
tirlochan016 <trilochan@crashnova.com>
2025-05-16 10:01:25 +0530

    added the sql query for registrations table:

--- db/db.go
+++ db/db.go
@@ -56,4 +56,22 @@ func createTables() {
 		fmt.Println(err)
 		panic("Could not create events table")
 	}
+
+	createRegistrationsTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_id INTEGER,
+			user_id INTEGER,
+			FOREIGN KEY (event_id) REFERENCES events(id),
+			FOREIGN KEY (user_id) REFERENCES users(id),
+		)	
+	`
+
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not create registrations table")
+	}
+
 }
