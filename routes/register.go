285c70687e1152cc7b1c0e0ba0a18578fd4fbda9
tirlochan016 <trilochan@crashnova.com>
2025-05-17 12:20:06 +0530

    added logic for registring user for the event

---
+++ routes/register.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse event ID",
+			"err":     err.Error(),
+		})
+
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "could not fetch the event",
+			"err":     err.Error(),
+		})
+
+		return
+	}
+
+	err = event.Register(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "could not register for the event",
+			"err":     err.Error(),
+		})
+
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "registerd",
+	})
+}
+
+func cancelRegistration(context *gin.Context) {
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -15,6 +15,7 @@ func RegisterRoutes(server *gin.Engine) {
 	authenticated.POST("/events", createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events/:id/register", registerForEvent)
 
 	server.POST("/signup", signup)
 	server.POST("/login", login)
